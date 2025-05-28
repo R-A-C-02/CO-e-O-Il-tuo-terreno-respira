aa6353f0af6c4aced5faf1be1a69ea86a04c43ff
Melania Giorgini <melania.giorgini02@gmail.com>
2025-05-28 18:30:33 +0200

    modificato file get_meteo di nuovo

--- BackEnd/app/get_meteo.py
+++ BackEnd/app/get_meteo.py
@@ -1,30 +1,28 @@
 import requests
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 import os
 from dotenv import load_dotenv
-from models import Plot, WeatherData
+from models import WeatherData
 from parte_finale_connect_db import recupero_coords_geocentroide
 
-#PLOT_ID = 1 # VA PRESO TRAMITE UNA CHIAMATA AL FRONTEND
+coords = list(recupero_coords_geocentroide().values())
+# {'plot_id': 1, 'latitudine': 41.8902, 'longitudine': 12.4924}
 
-def get_session():
+plot_id = coords[0]["plot_id"]
+latitude = coords[0]["latitudine"]
+longitude = coords[0]["longitudine"]
+
+def fetch_and_save_weather_day(plot_id, lat, lon):
+
     # DB CONNECTION
     load_dotenv()
     DATABASE_URL = os.getenv("DATABASE_URL")
     engine = create_engine(DATABASE_URL)
     Session = sessionmaker(bind=engine)
-    return Session()
+    session = Session()
 
-coords = recupero_coords_geocentroide().values()
-#{1: {'plot_id': 1, 'latitudine': 41.8902, 'longitudine': 12.4924}}
-coords_list = list(coords)
-plot_id = coords_list[0]["plot_id"]
-latitude = coords_list[0]["latitudine"]
-longitude = coords_list[0]["longitudine"]
-
-def fetch_and_save_weather_day(session, plot_id, lat, lon):
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
         "latitude": lat,
@@ -39,9 +37,7 @@ def fetch_and_save_weather_day(session, plot_id, lat, lon):
         print(f"❌ Errore nella richiesta meteo: {response.status_code}")
         return False
 
-    content = response.json()
-
-    hourly = content["hourly"]
+    hourly = response.json()["hourly"]
 
     # PARSING & SAVING
     for i in range(len(hourly["time"])):
@@ -58,6 +54,7 @@ def fetch_and_save_weather_day(session, plot_id, lat, lon):
         session.add(weather)
 
     session.commit()
+    session.close()
     print("✅ Dati meteo salvati nel database.")
     return True
 
@@ -75,19 +72,16 @@ def fetch_weather_week(plot_id, lat, lon):
         print(f"❌ Errore nella richiesta meteo: {response.status_code}")
         return False
 
-    content = response.json()
-
-    daily = content["daily"]
-
+    daily = response.json()["daily"]
     data = []
 
     for i in range(len(daily["time"])):
         data.append({
             "date": daily["time"][i],
-            "temperature": daily["temperature_2m"][i],
-            "humidity": daily["relative_humidity_2m"][i],
-            "precipitation": daily["precipitation"][i],
-            "radiation": daily["shortwave_radiation"][i]
+            "temperature": daily["temperature_2m_mean"][i],
+            "humidity": daily["relative_humidity_2m_mean"][i],
+            "precipitation": daily["precipitation_sum"][i],
+            "radiation": daily["shortwave_radiation_sum"][i]
         })
 
     print("✅ Dati meteo settimanali ottenuti.")
